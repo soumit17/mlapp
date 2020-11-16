81ec69e87b99a5059811bb7992d84215ac01c980
soumit <soumit_bhowmick@hotmail.com>
2020-11-16 11:32:41 +0400

    object detection api added

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ import tensorflow.keras
 from PIL import Image, ImageOps
 import io
 import base64
+import cv2
+import tensorflow as tf
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
 
 app=Flask(__name__)
 Swagger(app)
@@ -18,6 +22,21 @@ np.set_printoptions(suppress=True)
 model_cardam = tensorflow.keras.models.load_model('/home/ubuntu/mlapp/keras_model.h5')
 # model_cardam = tensorflow.keras.models.load_model('keras_model.h5')
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+detect_fn = tf.saved_model.load('/home/ubuntu/mlapp/saved_model')
+PATH_TO_LABELS=r"/home/ubuntu/mlapp/saved_model/label_map.pbtxt"
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+def B64argtoCV2(argname):
+    inputdata = request.form[argname]
+    im_bytes = base64.b64decode(bytes(str(inputdata)[2:], 'ascii'))
+    im_arr = np.frombuffer(im_bytes, dtype=np.uint8)  # im_arr is one-dim Numpy array
+    return cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
+
+def CV2toB64(image):
+    _, im_arr = cv2.imencode('.jpg', image)  # im_arr: image in Numpy one-dim array format.
+    im_bytes = im_arr.tobytes()
+    return base64.b64encode(im_bytes)                                                             
 
 @app.route('/insurance_cost')
 def predict():
@@ -113,5 +132,43 @@ def predict_car_damage():
     data[0] = normalized_image_array
     prediction = model_cardam.predict(data)
     return str('Car is Damaged!' if prediction[0][0]>.5 else 'Car is Good!')
+    
+def garbage():
+    """Identify litter via object detection
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: imageB64
+        in: formData
+        type: text
+        required: true
+      
+    responses:
+        200:
+            description: Output result
+        
+    """
+    image = B64argtoCV2('imageB64')
+    input_tensor = tf.convert_to_tensor(image)
+    input_tensor = input_tensor[tf.newaxis, ...]
+    detections = detect_fn(input_tensor)
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                   for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+    image_with_detections = image.copy()
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+      image_with_detections,
+      detections['detection_boxes'],
+      detections['detection_classes'],
+      detections['detection_scores'],
+      category_index,
+      use_normalized_coordinates=True,
+      max_boxes_to_draw=200,
+      min_score_thresh=0.6,
+      agnostic_mode=False)
+    return CV2toB64(image_with_detections)
+    
 if __name__=='__main__':
     app.run()
